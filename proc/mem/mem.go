0b3a898d52a03455ac42ff0601146bdc2161b146
Sean Wilson <spwilson27@gmail.com>
2016-04-14 22:30:55 -0500

    Reads whole binary files in to memory with func Fill

--- proc/mem/mem.go
+++ proc/mem/mem.go
@@ -1,1 +1,31 @@
 package mem
+
+import (
+	//"fmt"
+	"bytes"
+	"io/ioutil"
+	"encoding/binary"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func read_int32(data []byte) (ret uint32) {
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+/* Fill in the given array with the input file's data.*/
+func Fill(inputfile string, arr *[]uint32) {
+
+	data, err := ioutil.ReadFile(inputfile)
+	check(err)
+
+	for val := 0 ; val < len(data) / 4; val += 4 {
+		*arr = append(*arr, read_int32(data[val*4:val*4+4]))
+	}
+}
--- riscy-pemu.go
+++ riscy-pemu.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"github.com/spwilson2/riscy-pemu/proc/mem"
 	"flag"
+	"fmt"
 )
 
 func main() {
-	cli()
+	inputfilename := flag.String("i", "", "The input file path") 
+	dumpfilename := flag.String("m", "default", "File to dump memory to")
+	dumpashex := flag.Bool("h", false, "Pipe memory dump into a hex formater")
+	dumpregs := flag.Bool("r", false, "Dump the register file with stats.")
+	printcounts := flag.Bool("c", false, "Print count of each executed instruction")
+	quiet := flag.Bool("q", true, "Don't print stats")
+	detailed := flag.Bool("detailed", false, "Stats are very detailed")
+
+	flag.Parse()
+
+	_ = dumpfilename
+	_ = inputfilename
+	_ = dumpashex
+	_ = dumpregs
+	_ = printcounts
+	_ = quiet
+	_ = detailed
+
+	if *inputfilename == "" {
+		fmt.Println("Must supply an input file path.")
+		fmt.Println("")
+		flag.PrintDefaults()
+		return
+	}
+
+	var memory []uint32
+
+	mem.Fill(*inputfilename, &memory)
+	for _, val := range memory {
+		fmt.Printf("0x%x\n", val)
+	}
 }
 
 /*
@@ -22,6 +54,4 @@ Arguments are:
 * --detailed : Stats are very detailed.
 */
 func cli() {
-	dumpfilename := flag.String("m", "", "File to dump memory to")
-	inputfilename := flag.String("i", "", "The input file path")
 }
